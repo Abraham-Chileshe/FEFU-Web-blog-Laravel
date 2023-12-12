889b013cebcb99758a38f32db50564fe5891be41
Chileshe <64306558+Abraham-Chileshe@users.noreply.github.com>
2023-12-13 03:57:25 +1000

    Create footer.blade.php

---
+++ blog/resources/views/blog/inc/footer.blade.php
@@ -0,0 +1,25 @@
+<!-- Bootstrap -->
+<footer class="text-center text-lg-start">
+    <div class="container d-flex justify-content-center py-5">
+        <button type="button" class="btn btn-dark btn-lg btn-floating mx-2">
+            <i class="fa fa-camera"></i>
+        </button>
+        <button type="button" class="btn btn-dark btn-lg btn-floating mx-2">
+            <i class="fa fa-phone"></i>
+        </button>
+        <button type="button" class="btn btn-dark btn-lg btn-floating mx-2">
+            <i class="fa fa-envelope"></i>
+        </button>
+    </div>
+
+    <!-- Copyright -->
+    <div class="text-center text-white p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+        <span>© 2023 Abraham Chileshe</span>
+    </div>
+</footer>
+
+<!-- // Bootstrap -->
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
