7b7f1a02c8dac087e1088ca15a58d15aace00b93
Chileshe <64306558+Abraham-Chileshe@users.noreply.github.com>
2023-12-13 03:57:49 +1000

    Update web.php

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -31,3 +31,12 @@ Route::post('add-user','RegistrationController@store');
 Route::get('login-user', 'SessionsController@create');
 Route::post('login-user', 'SessionsController@store');
 Route::get('logout-user', 'SessionsController@destroy');
+
+Route::get('lang-update', 'DML@lang_update');
+
+
+Route::get('add-post', 'AddPost@postinsert');
+Route::post('add-post','AddPost@postinsert');
+
+
+Route::get('home', 'AddPost@viewpost');
