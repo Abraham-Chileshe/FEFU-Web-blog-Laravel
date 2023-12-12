f8d121dcdc08bcc8cb4fa8f2792eee905f7cca13
Chileshe <64306558+Abraham-Chileshe@users.noreply.github.com>
2023-12-13 03:56:05 +1000

    Create DML.php

---
+++ blog/app/Http/Controllers/DML.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use DB;
+
+class DML extends Controller
+{
+
+    public function lang_update(){
+        if(auth()->user()->lang == "en")
+            $lang = "ru";
+        else
+            $lang = "en";
+
+        $data=array('lang'=>$lang);
+        DB::table('users')->where('id', auth()->user()->id)->update($data);
+
+        return redirect()->to('/home');
+    }
+}
