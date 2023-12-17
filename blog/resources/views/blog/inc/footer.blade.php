82744456182edd82020e4e6ba77199d7b8671e1a
Chileshe <64306558+Abraham-Chileshe@users.noreply.github.com>
2023-12-18 00:20:37 +1000

    Update footer.blade.php

--- blog/resources/views/blog/inc/footer.blade.php
+++ blog/resources/views/blog/inc/footer.blade.php
@@ -1,25 +1,9 @@
-<!-- Bootstrap -->
-<footer class="text-center text-lg-start">
-    <div class="container d-flex justify-content-center py-5">
-        <button type="button" class="btn btn-dark btn-lg btn-floating mx-2">
-            <i class="fa fa-camera"></i>
-        </button>
-        <button type="button" class="btn btn-dark btn-lg btn-floating mx-2">
-            <i class="fa fa-phone"></i>
-        </button>
-        <button type="button" class="btn btn-dark btn-lg btn-floating mx-2">
-            <i class="fa fa-envelope"></i>
-        </button>
-    </div>
-
-    <!-- Copyright -->
-    <div class="text-center text-white p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-        <span>© 2023 Abraham Chileshe</span>
-    </div>
+<!-- Footer -->
+<footer class="bg-dark text-light text-center py-3 mt-4" style="min-height:100hv">
+    <p>&copy; 2023 Your Website</p>
 </footer>
 
-<!-- // Bootstrap -->
-
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+<!-- Bootstrap Scripts -->
+<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.0/jquery.min.js'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js'></script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css"/>
