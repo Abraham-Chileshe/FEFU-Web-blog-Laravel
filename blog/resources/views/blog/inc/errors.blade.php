18c02aae0daa7c91850c402e0622911809c9edff
Chileshe <64306558+Abraham-Chileshe@users.noreply.github.com>
2023-12-27 00:28:45 +1000

    Create errors.blade.php

---
+++ blog/resources/views/blog/inc/errors.blade.php
@@ -0,0 +1,62 @@
+
+@if($errors->any())
+
+    <script>
+        $(document).ready(function(){
+            $(document).ready(function (){
+                $('#error').modal('show');
+            });
+        });
+    </script>
+
+@endif
+
+{{--@if($showModal)--}}
+
+   
+
+    <!-- Modal -->
+    <div class="modal fade" id="error" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title text-danger " id="exampleModalLabel">Failed</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    {{ $errors->first('error') }}
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+<!-- Modal -->
+<div class="modal fade" id="success" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title text-primary " id="exampleModalLabel">Failed to Register</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                {{ $errors->first('error') }}
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+    <!-- JavaScript to automatically trigger the modal -->
