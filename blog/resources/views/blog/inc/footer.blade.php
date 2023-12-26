56c006d5903ba76337b73de54635220475e9a1f6
Chileshe <64306558+Abraham-Chileshe@users.noreply.github.com>
2023-12-27 00:28:59 +1000

    Update footer.blade.php

--- blog/resources/views/blog/inc/footer.blade.php
+++ blog/resources/views/blog/inc/footer.blade.php
@@ -4,6 +4,7 @@
 </footer>
 
 <!-- Bootstrap Scripts -->
-<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.0/jquery.min.js'></script>
-<script src='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js'></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous" ></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css"/>
