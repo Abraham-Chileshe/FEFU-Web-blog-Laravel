9625de206710f284a10778a380f8ae1b2d650acf
Chileshe <64306558+Abraham-Chileshe@users.noreply.github.com>
2023-12-27 02:42:26 +1000

    Update view_post.blade.php

--- blog/resources/views/blog/view_post.blade.php
+++ blog/resources/views/blog/view_post.blade.php
@@ -57,7 +57,6 @@ $posts = DB::table('posts')
             <!-- Scrollable Block -->
             <div class="col-md-8">
 
-
                 <div class="scroll-container" style="height:1000px">
                     @php
                        
@@ -71,7 +70,7 @@ $posts = DB::table('posts')
 
                     @endif
 
-                        @foreach ($posts as $post)
+                    @foreach ($posts as $post)
                         <div class="mt-3 card main-card">
                             <div class="post">
                                 @if( auth()->check() )
@@ -98,129 +97,118 @@ $posts = DB::table('posts')
                                 @endphp
 
                                 @if( auth()->check() )
-                                    <!-- Update the like button link -->
-
                                 
-                                    <span class="mt-3 update_like{{$pid}}">
-                                    <a href="#" class="btn mt-2 like-btn" id="like_{{ $pid }}" style="min-width:70px">
-                                        <i class="mr-1 fa fa-heart"></i> {{ $num_likes }}
-                                    </a>
-
-                                   
-                                    </span>
-                                    <hr/>
-                                   
-                                  
-                                
+                                    {{--Like, coomment and read more button when logged in--}}
+                                    <div class="form-group" >
+                                        <span class="mt-3 update_like{{$pid}}">
+                                            <a href="#" class="btn mt-2 like-btn" id="like_{{ $pid }}" style="min-width:70px"><i class="mr-1 fa fa-heart"></i> {{ $num_likes }}</a>
+                                        </span><hr/>
+                                    </div>
+                                    {{--Like, coomment and read more button when logged in--}}
+                                    
                                 @else
-                                    <a class="btn"  style="background-color: var(--blue-green); min-width:70px">
-                                        <i class="ml-2 fa fa-heart"></i> {{ $num_likes }}
-                                    </a>
+                                  {{--Like, coomment and read more button when NOT logged in--}}
+                                  <div class="form-group" >
+                                    <a class="btn mt-2"  style="background-color: var(--blue-green); min-width:70px"><i class="ml-2 fa fa-heart"></i> {{ $num_likes }}</a>
+                                  </div>
+                                  {{--Like, coomment and read more button when NOT logged in--}}
 
                                 @endif
 
                                 <h5 class="mt-4 mb-2">{{ $post->title}}</h5>
-
                                 <p>{{$post->description}}</p>
-
-
-                               
-
                                                                                             
                             </div>
-
                         </div>
-                        @endforeach
+                    @endforeach
                    
                 </div>
             </div>
-
-           
+            <!-- Scrollable Block -->
 
             
-
-
-        
-
-
             <!-- Right Column -->
             <div class="col-md-4">
                 <div class="">
                     <div class="mt-3 card main-card">
                         <div class="container">
-                        <h4 class="mt-2 comment_btn">Comments -  {{ $num_comments }}  </h4>
-                        <hr/>
+                            <h4 class="mt-2 comment_btn">Comments -  {{ $num_comments }}  </h4>
+                            <hr/>
                         
-                        <div id="all_coms" class="comment-thread  scroll-container" style="height:800px">
-                        
-                        @if ($comments->isEmpty()) 
-                            <div class="container" style="margin-top:5em; max-width:700px">
-                            <h5 style="margin-top:4em; line-height:22px; font-family:Arial,Helvetica,sans-serif; padding-top:5em; padding-bottom:2em; color: grey; text-align: center;">No comments</h5>
-                            <h3 style="margin-top:-1em; color:white; background-color: rgba(34, 34, 34, 0.5); text-align: center;" class="rounded"><i class="fa fa-exclamation-triangle" style="margin: 0 auto;"></i></h3><br/>
-                            </div>
-                        @endif
+                            @if( auth()->check() )
+                                <div id="all_coms" class="comment-thread  scroll-container" style="height:800px">
+                                    @if ($comments->isEmpty()) 
+                                        <div class="container" style="margin-top:5em; max-width:700px">
+                                        <h5 style="margin-top:4em; line-height:22px; font-family:Arial,Helvetica,sans-serif; padding-top:5em; padding-bottom:2em; color: grey; text-align: center;">No comments</h5>
+                                        <h3 style="margin-top:-1em; color:white; background-color: rgba(34, 34, 34, 0.5); text-align: center;" class="rounded"><i class="fa fa-exclamation-triangle" style="margin: 0 auto;"></i></h3><br/>
+                                        </div>
+                                    @endif
 
-                        @foreach ($comments as $comment)
-                            <!-- Comment 1 start -->
-                            <div class="comment" id="comment-1">
-                                <a href="#comment-1" class="comment-border-link">
-                                    <span class="sr-only">Jump to comment-1</span>
-                                </a>
-                                <div class="comment-heading">
-                                    <div class="comment-voting">
-                                        <button type="button">
-                                            <span aria-hidden="true">&#9650;</span>
-                                            <span class="sr-only">Vote up</span>
-                                        </button>
-                                        <button type="button">
-                                            <span aria-hidden="true">&#9660;</span>
-                                            <span class="sr-only">Vote down</span>
-                                        </button>
-                                    </div>
-                                    <div class="comment-info">
-                                        <a href="#" class="comment-author"  style="text-transform: capitalize;">{{ $comment->commentern}}</a>
-                                        <p class="m-0">
-                                            &bull; {{ $comment->added_at}}
-                                        </p>
-                                    </div>
-                                </div>
-                        
-                                <div class="comment-body">
-                                    <p>
-                                        {{ $comment->text}}
-                                     </p>
-                                    <button type="button">Like</button>
+                                    @foreach ($comments as $comment)
+                                        <!-- Comment 1 start -->
+                                        <div class="comment" id="comment-1">
+                                            <a href="#comment-1" class="comment-border-link">
+                                                <span class="sr-only">Jump to comment-1</span>
+                                            </a>
+                                            <div class="comment-heading">
+                                                <div class="comment-voting">
+                                                    <button type="button">
+                                                        <span aria-hidden="true">&#9650;</span>
+                                                        <span class="sr-only">Vote up</span>
+                                                    </button>
+                                                    <button type="button">
+                                                        <span aria-hidden="true">&#9660;</span>
+                                                        <span class="sr-only">Vote down</span>
+                                                    </button>
+                                                </div>
+                                                <div class="comment-info">
+                                                    <a href="#" class="comment-author"  style="text-transform: capitalize;">{{ $comment->commentern}}</a>
+                                                    <p class="m-0">
+                                                        &bull; {{ $comment->added_at}}
+                                                    </p>
+                                                </div>
+                                            </div>
                                     
+                                            <div class="comment-body">
+                                                <p>
+                                                    {{ $comment->text}}
+                                                </p>
+                                                <button type="button">Like</button>
+                                                
+                                            </div>
+                                    
+                                        
+                                        </div>
+                                        <!-- Comment 1 end -->
+
+                                    @endforeach
                                 </div>
-                        
-                               
-                            </div>
-                            <!-- Comment 1 end -->
+                                
+                                <div class="form-group mt-3" >
+                                    <form method="post" class="comment_form" id="comment_{{ $pid }}">
+                                        <input type="text" class="form-control" placeholder="Comment" />
+                                        <input type="submit" class="btn btn-info mt-2" value="submit" />
+                                    </form>
+                                </div>
 
-                        @endforeach
-                        </div>
+                            @else
+                                <div id="all_coms" class="comment-thread " style="height:800px">
+                                    <div class="container" style="margin-top:7em; max-width:700px">
+                                        <h5 style="margin-top:4em; line-height:22px; font-family:Arial,Helvetica,sans-serif; padding-top:5em; padding-bottom:2em; color: grey; text-align: center;">You need to register to view comments</h5>
+                                        <h3 style="margin-top:-1em; color:white; background-color: rgba(34, 34, 34, 0.5); text-align: center;" class="rounded"><i class="fa fa-exclamation-triangle" style="margin: 0 auto;"></i></h3><br/>
+                                    </div>
+                                </div>
 
-                        <div class="form-group mt-3" >
-                            <form method="post" class="comment_form" id="comment_{{ $pid }}">
-                                <input type="text" class="form-control" placeholder="Comment" />
-                                <input type="submit" class="btn btn-info mt-2" value="submit" />
-                            </form>
-                        </div>
-                        
+                            @endif
                             
-                       
 
+                           
+                    
                         </div>
                     </div>
-                </div>
-
-
-               
-
-                
-            
-                  
+                </div>   
             </div>
+            
 
              {{-- This update the like and Comment button very second--}}
              <script>
