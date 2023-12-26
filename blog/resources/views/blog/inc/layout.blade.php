e9a13f1623c07d68811253be8cabd0999095a44d
Chileshe <64306558+Abraham-Chileshe@users.noreply.github.com>
2023-12-27 00:29:17 +1000

    Update layout.blade.php

--- blog/resources/views/blog/inc/layout.blade.php
+++ blog/resources/views/blog/inc/layout.blade.php
@@ -3,11 +3,14 @@
 <head>
     <title>Your Bootstrap Website</title>
     <meta charset="UTF-8">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/login_style.css') }}" rel="stylesheet">
     <script src="{{ asset('languages/my_profile.js') }}"></script>
+
 </head>
 <body>
 
