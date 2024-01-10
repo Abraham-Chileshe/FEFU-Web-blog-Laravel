dca23426ed5fd7bcbe55ee9a83d836aa4b3a925d
Chileshe <64306558+Abraham-Chileshe@users.noreply.github.com>
2024-01-10 23:56:03 +1000

    Update web.php

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -11,13 +11,17 @@
 |
 */
 
+
 Route::get('/', function () {
     return view('blog.home');
 });
 
-
 Route::get('/post', function () {
     return view('blog.view_post');
+});
+
+Route::get('/admin', function () {
+    return view('blog.admin');
 });
 
 
@@ -31,20 +35,32 @@ Route::post('add-user','RegistrationController@store');
 Route::get('login', 'AuthController@loginUser');
 Route::post('login', 'AuthController@loginUser');
 
+Route::get('admin-login', 'AdminController@loginAdmin');
+Route::post('admin-login', 'AdminController@loginAdmin');
+
 Route::get('logout','AuthController@destroy');
 Route::get('lang-update', 'DML@lang_update');
-Route::post('agreement', 'DML@agree');
+
 
 
 
 Route::get('add-post', 'AddPost@postinsert');
 Route::post('add-post','AddPost@postinsert');
 
+Route::get('delete', 'DML@delete');
+Route::get('delete-user', 'DML@delete_user');
+Route::get('add-admin', 'DML@add_admin');
+Route::get('remove-admin', 'DML@remove_admin');
+Route::post('delete', 'DML@delete');
+
 Route::get('add-like', 'AddLike@Like');
 Route::post('add-like','AddLike@Like');
 
 Route::get('add-comment', 'AddComment@Comment');
 Route::post('add-comment','AddComment@Comment');
+
+Route::get('add-survey', 'AddSurvey@Survey');
+Route::post('add-survey','AddSurvey@Survey');
 
 
 
