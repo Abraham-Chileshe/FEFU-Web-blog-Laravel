e741239c2b3c895dc0b74e94c45ac98a81994fb1
Chileshe <64306558+Abraham-Chileshe@users.noreply.github.com>
2024-01-11 00:00:24 +1000

    Update DML.php

--- blog/app/Http/Controllers/DML.php
+++ blog/app/Http/Controllers/DML.php
@@ -20,12 +20,72 @@ class DML extends Controller
         return redirect()->to('/');
     }
 
-    public function agree(){
-        $data=array('new_acc'=>0);
-        DB::table('users')->where('id', auth()->user()->id)->update($data);
+    public function delete(Request $request){
+        $postid = $request->input('postid');
+        
+        try {
+            // Insert the user into the database
+            DB::table('posts')->where('id', $postid)->delete();
+            return redirect()->to('/');
 
-        return redirect()->to('/');
+        } catch (\Illuminate\Database\QueryException $e) {
+            return('failed');
+        }
+    }
+
+    public function delete_user(Request $request){
+        $user_id = $request->input('userid');
+        
+        try {
+            // Insert the user into the database
+            DB::table('users')->where('id', $user_id)->delete();
+            DB::table('admins')->where('user_id', $user_id)->delete();
+            DB::table('posts')->where('user_id', $user_id)->delete();
+            DB::table('survey')->where('user_id', $user_id)->delete();
+            DB::table('likes')->where('Liker', $user_id)->delete();
+            DB::table('comments')->where('commenter', $user_id)->delete();
+            return redirect()->to('/admin');
+
+        } catch (\Illuminate\Database\QueryException $e) {
+            return('failed');
+        }
+      
+    }
+
+    public function add_admin(Request $request){
+        $user_id = $request->input('userid');
+        
+        $data = array(
+            'user_id' => $user_id,
+            
+        );
+      
+
+        try {
+            // Insert the user into the database
+            DB::table('admins')->insert($data);
+            return redirect()->to('/admin');
+
+        } catch (\Illuminate\Database\QueryException $e){
+            return('Failed to Add');
+        }
+    }
+
+    public function remove_admin(Request $request){
+        $user_id = $request->input('userid');
+
        
+
+        try {
+            // Insert the user into the database
+            DB::table('admins')->where('user_id', $user_id)->delete();
+            return redirect()->to('/admin');
+
+        } catch (\Illuminate\Database\QueryException $e) {
+            return('failed');
+        }
+        
+        
     }
 
     
